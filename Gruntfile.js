4684ba280da915dc0dabf080f5470b1786f2c47b
cwklausing <cwklausing@gmail.com>
2017-06-05 13:14:10 -0500

    add babel and rollup tooling to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,30 @@
+var babel = require('rollup-plugin-babel');
+var resolve = require('rollup-plugin-node-resolve');
+
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		rollup: {
+			options: {
+				format: 'iife',
+				plugins: function() {
+					return [
+						babel({
+							exclude: './node_modules/**'
+						})
+					];
+				}
+			},
+			dist: {
+				files: {
+					'assets/js/dist/bundle.js': ['assets/js/app.js']
+				}
+			}
+		},
 		sass: {
 			dist: {
 				files: {
-					'assets/css/app.css' : 'assets/sass/app.scss'
+					'assets/css/app.css': 'assets/sass/app.scss'
 				}
 			}
 		},
@@ -12,18 +32,16 @@ module.exports = function(grunt) {
 			css: {
 				files: '**/*.scss',
 				tasks: ['sass']
+			},
+			js: {
+				files: ['assets/js/dist/app.js', 'assets/js/*.js'],
+				tasks: ['rollup']
 			}
-		},
-		uglify: {
-			my_target: {
-      	files: {
-        	'assets/css/app.css': 'assets/sass/app.scss'
-	      }
-	    }
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-rollup');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('default',['watch', 'uglify']);
-}
+	grunt.registerTask('default', ['watch', 'uglify']);
+};
